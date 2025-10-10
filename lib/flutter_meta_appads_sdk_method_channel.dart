6339982eadb8e7fc4a9cbd79542d0bbb1f67da9c
Sergio Durban <sdurban@leadtech.com>
2025-10-11 00:00:25 +0200

    fix: Dart analyze problems

--- lib/flutter_meta_appads_sdk_method_channel.dart
+++ lib/flutter_meta_appads_sdk_method_channel.dart
@@ -7,54 +7,54 @@ import 'flutter_meta_appads_sdk_platform_interface.dart';
 class MethodChannelFlutterMetaAppadsSdk extends FlutterMetaAppadsSdkPlatform {
   /// The Pigeon API used to interact with the native platform.
   @visibleForTesting
-  final FlutterMetaAppadsSdkHostApi _api = FlutterMetaAppadsSdkHostApi();
+  final FlutterMetaAppadsSdkHostApi api = FlutterMetaAppadsSdkHostApi();
 
   @override
   Future<void> initSdk({bool enableLogging = false}) async {
-    await _api.initSdk(enableLogging);
+    await api.initSdk(enableLogging);
   }
 
   @override
   Future<void> setUserData(FBSetUserDataRequest request) async {
-    await _api.setUserData(request);
+    await api.setUserData(request);
   }
 
   @override
   Future<void> logStandardEvent(
       FBLogStandardEventRequest request) async {
-    await _api.logStandardEvent(request);
+    await api.logStandardEvent(request);
   }
 
   @override
   Future<void> logPurchase(FBLogPurchaseRequest request) async {
-    await _api.logPurchase(request);
+    await api.logPurchase(request);
   }
 
   @override
   Future<void> logEvents(FBLogEventRequest request) async {
-    await _api.logEvents(request);
+    await api.logEvents(request);
   }
 
   @override
   Future<FBAnonIdResponse?> getFbAnonId() async {
-    return await _api.getFbAnonId();
+    return await api.getFbAnonId();
   }
 
   Future<void> setAdvertiserTrackingEnabled({required bool isEnabled}) async {
-    await _api.setAdvertiserTrackingEnabled(isEnabled);
+    await api.setAdvertiserTrackingEnabled(isEnabled);
   }
 
   Future<void> setAdvertiserIDCollectionEnabled(
       {required bool isEnabled}) async {
-    await _api.setAdvertiserIDCollectionEnabled(isEnabled);
+    await api.setAdvertiserIDCollectionEnabled(isEnabled);
   }
 
   Future<void> setAutoLogAppEventsEnabled({required bool isEnabled}) async {
-    await _api.setAutoLogAppEventsEnabled(isEnabled);
+    await api.setAutoLogAppEventsEnabled(isEnabled);
   }
 
   Future<void> setDataProcessingOptions(
       FBSetDataProcessingOptionsRequest request) async {
-    await _api.setDataProcessingOptions(request);
+    await api.setDataProcessingOptions(request);
   }
 }
